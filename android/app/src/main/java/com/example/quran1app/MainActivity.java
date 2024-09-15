5c98427b5b648d2ad92d5e625708a3861c8e38b8
user <uuytt712@gmail.com>
2024-09-15 14:57:58 +0300

    init project.

---
+++ android/app/src/main/java/com/example/quran1app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.quran1app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:quran1app/homescreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: HomeScreen.routName,
+      routes: {HomeScreen.routName: (context) => HomeScreen()},
+    );
+  }
+}
